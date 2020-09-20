f1a34aa21b0fec51ea60ba479e38b3ae4baaadbe
Gian Paul Moreno <gipamo0230@gmail.com>
2020-09-20 14:24:01 -0500

    navbar componente

--- maderexpo/src/App.js
+++ maderexpo/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ButtonsRelation from './componets/ButtonsRelation';
+import Navbar from './componets/Navbar';
 import ProductFormulary from './componets/ProductFormulary';
 
 
@@ -14,14 +15,8 @@ function App() {
                 <button class="btn btn_dark">Language</button>
             </div>
             <h1 class="title_name">MADEREXPO</h1>
-            <div class="navBar">
-                <a href="">Inicio</a>
-                <a href="" id="position">Nuestras operaciones</a>
-                <a href="">Desarrollo sostenible</a>
-                <a href="" id="">Nuestro producto</a>
-                <a href="">Contáctanos</a>
-            </div>
-        </div>
+            <Navbar/>
+          </div>
       </header>
       <ButtonsRelation/>
       </div>
---
+++ maderexpo/src/componets/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div class="navBar">
+            <a href="">Inicio</a>
+            <a href="" id>Nuestras operaciones</a>
+            <a href="">Desarrollo sostenible</a>
+            <a href="" id="">Nuestro producto</a>
+            <a href="" id="position">Contáctanos</a>
+            </div>
+        )
+    }
+}
