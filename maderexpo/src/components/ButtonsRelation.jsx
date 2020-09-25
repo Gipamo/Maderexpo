a008fd843e0ffa002ee6f31df8f9491965e47f33
Andres <andres.ramos@upb.edu.co>
2020-09-25 16:15:06 -0500

    Eliminacion del buzon-vista relation

--- maderexpo/src/components/ButtonsRelation.jsx
+++ maderexpo/src/components/ButtonsRelation.jsx
@@ -11,8 +11,7 @@ function ButtonsRelation(){
                 <div class="div1"> <Link to="/map"> <img src={Icon} alt="Icono" /><br/><a href="">Sedes Fisicas</a> </Link></div>
                 <div class="div2"> <img src={Icon} alt="Icono" /><br/><a href="">Preguntas Frecuentes</a> </div>
                 <div class="div3"> <img src={Icon} alt="Icono" /><br/><a href="">Glosario</a> </div>
-                <div class="div4"> <img src={Icon} alt="Icono" /><br/><a href="">Buzon de Contacto</a> </div>
-                <div class="div5"> <img src={Icon} alt="Icono" /><br/><a href="">Buzon de Sugerencias</a> </div>
+                <div class="div4"> <img src={Icon} alt="Icono" /><br/><a href="">Buzon de Sugerencias</a> </div>
                 <div class="div6"> <Link to="/productform"> <img src={Icon} alt="Icono" /><br/><a href="">Preguntas del Producto</a> </Link></div>
                 </div>
             </div>
