b80497f7577e2ae19262c2420cb208cd833fa011
Gian Paul Moreno <gipamo0230@gmail.com>
2020-10-02 10:04:47 -0500

    Botones relacion mejorados

--- maderexpo/src/components/ButtonsRelation.jsx
+++ maderexpo/src/components/ButtonsRelation.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../css/App.css'
-import Icon from '../img/icono.png';
+import IconMap from '../img/icono.png';
+import IconQuestion from '../img/preguntas-mas-frecuentes.png';
+import IconGlosary from '../img/diccionario.png';
+import IconSugest from '../img/bueno.png';
+import IconProduct from '../img/inventario.png';
 import { Switch, Route,Link } from 'react-router-dom'
 
 function ButtonsRelation(){
@@ -8,11 +12,18 @@ function ButtonsRelation(){
         return(
             <div>
                 <div class="parent">
-                <div class="div1"> <Link to="/map"> <img src={Icon} alt="Icono" /><br/><a href="">Sedes Fisicas</a> </Link></div>
-                <div class="div2"> <img src={Icon} alt="Icono" /><br/><a href="">Preguntas Frecuentes</a> </div>
-                <div class="div3"> <img src={Icon} alt="Icono" /><br/><a href="">Glosario</a> </div>
-                <div class="div4"> <img src={Icon} alt="Icono" /><br/><a href="">Buzon de Sugerencias</a> </div>
-                <div class="div6"> <Link to="/productform"> <img src={Icon} alt="Icono" /><br/><a href="">Preguntas del Producto</a> </Link></div>
+                <div class="upButton">
+                    <div class="div1"> <Link to="/map"> <img src={IconMap} alt="Icono" /><br/><a href="">Sedes Fisicas</a> </Link></div>
+                    <div class="div2"> <img src={IconQuestion} alt="Icono" /><br/><a href="">Preguntas Frecuentes</a> </div>
+                    <div class="div3"> <img src={IconGlosary} alt="Icono" /><br/><a href="">Glosario</a> </div>  
+                </div>
+                <div class="downButton"> 
+                    <div class="div4"> <img src={IconSugest} alt="Icono" /><br/><a href="">Buzon de Sugerencias</a> </div>
+                    <div class="div6"> <Link to="/productform"> <img src={IconProduct} alt="Icono" /><br/><a href="">Preguntas del Producto</a> </Link></div>
+                
+                </div>   
+
+               
                 </div>
             </div>
         )
